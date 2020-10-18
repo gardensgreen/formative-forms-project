082831609b712743d1956cacb08e5732400398f7
Daniel TIllero <danieltillero67@gmail.com>
2020-10-18 16:46:43 -0400

    create user form submit

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const express = require("express");
 const csrf = require("csurf");
 const cookieParser = require("cookie-parser");
 const app = express();
-const csrfProtection = csrf({cookie: true});
-
+const csrfProtection = csrf({ cookie: true });
 
 const port = process.env.PORT || 3000;
 
@@ -18,6 +17,7 @@ const users = [
 
 app.set("view engine", "pug");
 app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
     // res.send("Hello World!");
@@ -25,8 +25,37 @@ app.get("/", (req, res) => {
 });
 
 app.get("/create", csrfProtection, (req, res, next) => {
-    res.render("create", { csrfToken: req.csrfToken() })
+    res.render("create", { csrfToken: req.csrfToken() });
+});
 
+let idCount = 1;
+app.post("/create", csrfProtection, (req, res) => {
+    const errors = [];
+    const { firstName, lastName, email, password } = req.body;
+
+    console.log(req.body);
+    if (!firstName) errors.push("Please fill out the first name field.");
+    if (!lastName) errors.push("Please fill out the last name field.");
+    if (!email) errors.push("Please fill out the email field.");
+    if (!password) errors.push("Please fill out the password field.");
+
+    const user = {
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        password: password,
+    };
+
+    if (errors.length > 0) {
+        res.render("create", { user, errors, csrfToken: req.csrfToken() });
+        return;
+    }
+
+    idCount++;
+    user.id = idCount;
+
+    users.push(user);
+    res.render("create", { csrfToken: req.csrfToken() });
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
