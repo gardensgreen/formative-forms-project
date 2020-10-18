3861e5366e550c426ddb1adace2cc6d5ba24bcd9
Daniel TIllero <danieltillero67@gmail.com>
2020-10-18 14:16:13 -0400

    Complete home page

--- index.js
+++ index.js
@@ -3,20 +3,21 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
+const users = [
+    {
+        id: 1,
+        firstName: "Jill",
+        lastName: "Jack",
+        email: "jill.jack@gmail.com",
+    },
+];
+
 app.set("view engine", "pug");
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+    // res.send("Hello World!");
+    res.render("index", { users });
 });
-
-const users = [
-  {
-    id: 1,
-    firstName: "Jill",
-    lastName: "Jack",
-    email: "jill.jack@gmail.com"
-  }
-];
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
