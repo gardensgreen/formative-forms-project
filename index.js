2322c3eb1f2e9931c426542d81532590d8deec26
Tanner Schmutte <tannerschmutte@gmail.com>
2020-10-21 11:12:29 -0700

    execute phase 2 put the turkey in the oven

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
-
+const csrf = require("csurf");
+const cookieParser = require("cookie-parser");
 const app = express();
+const csrfProtection = csrf({cookie: true});
+
+
 const port = process.env.PORT || 3000;
 
 const users = [
@@ -13,10 +17,16 @@ const users = [
 ];
 
 app.set("view engine", "pug");
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
     // res.send("Hello World!");
     res.render("index", { users });
+});
+
+app.get("/create", csrfProtection, (req, res, next) => {
+    res.render("create", { csrfToken: req.csrfToken() })
+
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
